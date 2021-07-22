1f483e2e0fcd379b2f5aa9bbdb59dc12d7be4aa3
Jorge Alba <jorge.alba@epn.edu.ec>
2021-07-22 10:20:24 -0500

    Controlador y Rutas para Interest

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InterestController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,6 +22,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'authenticate']);
+
+//Rutas para Interest
+Route::get('interests',[InterestController::class, 'index']);
+Route::get('interests/{interest}',[InterestController::class, 'show']);
+Route::put('interests/{interest}',[InterestController::class, 'update']);
+Route::delete('interests/{interest}',[InterestController::class, 'delete']);
+
+
 
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::get('user', [UserController::class, 'getAuthenticatedUser']);
