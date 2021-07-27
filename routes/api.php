420b830d5ee57c0c9b6fc526216858d57698a9ee
Byron Huaraca <byron.huaraca@epn.edu.ec>
2021-07-26 23:11:42 -0500

    Rutas y controladores para Contact

--- routes/api.php
+++ routes/api.php
@@ -31,5 +31,11 @@ Route::delete('contacts/{contact}', [ContactController::class, 'delete']);
 
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::get('user', [UserController::class, 'getAuthenticatedUser']);
+
+    //contacts
+    Route::get('contacts', [ContactController::class, 'index']);
+    Route::get('contacts/{contact}', [ContactController::class, 'show']);
+  //Route::put('contacts/{contact}', [ContactController::class, 'update']);
+    Route::delete('contacts/{contact}', [ContactController::class, 'delete']);
 });
 
